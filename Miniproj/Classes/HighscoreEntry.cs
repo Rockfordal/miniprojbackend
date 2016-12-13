a5bb7f4aeb7bdb42963ff4cf598f55466a3c6c95
David-Lundmark <david.lundmarkbredband.net>
2016-12-13 14:12:40 +0100

    Highscore-tjosan

---
+++ Miniproj/Classes/HighscoreEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Miniproj.Classes
+{
+    public class HighscoreEntry
+    {
+        public int UserId { get; set; }
+        public int Score { get; set; }
+    }
+}
