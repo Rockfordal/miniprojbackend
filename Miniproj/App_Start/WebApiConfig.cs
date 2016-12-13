b4cd2464f43bff350ce8eaa053e18cb59d29985b
Anders Lundström <lundstrom123@gmail.com>
2016-12-13 16:06:25 +0100

    port 60000 med mera

--- Miniproj/App_Start/WebApiConfig.cs
+++ Miniproj/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace Miniproj
 {
@@ -19,6 +20,11 @@ namespace Miniproj
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            // Enable CORS
+            //var cors = new EnableCorsAttribute("localhost:8080,http://127.0.0.1:8080,http://10.23.192.246:60000,10.23.192.247,10.23.192.249", "*", "*");
+            //config.EnableCors(cors);
+            config.EnableCors();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
