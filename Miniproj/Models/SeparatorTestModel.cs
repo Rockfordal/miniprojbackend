ca551c12596a4e609e3212386a310ed6427539e5
David-Lundmark <david.lundmarkbredband.net>
2016-12-13 11:59:45 +0100

    Modeller

---
+++ Miniproj/Models/SeparatorTestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Miniproj.Models
+{
+    public class SeparatorTestModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(3)]
+        public string Text { get; set; }
+    }
+}
---
+++ Miniproj/Models/TextTestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Miniproj.Models
+{
+    public class TextTestModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(3)]
+        public string Text { get; set; }
+    }
+}
