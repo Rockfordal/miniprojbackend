c1c1e03f2dc7ffe5a271483e335d959b3d5cad03
Anders Lundström <lundstrom123@gmail.com>
2016-12-13 16:02:43 +0100

    test controller

---
+++ Miniproj/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//using System.Web.Http.Cors;10.23.192.246
+
+namespace Miniproj.Controllers
+{
+    //[EnableCors(origins: "http://mywebclient.azurewebsites.net", headers: "*", methods: "*")]
+    public class TestController : ApiController
+    {
+        // GET api/test
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "hej", "hoj" };
+        }
+    }
+}
