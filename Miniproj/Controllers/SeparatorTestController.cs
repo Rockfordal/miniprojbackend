2458b775c82d671fd5a97ac54ea61651d6dbfdd4
Anders Lundström <lundstrom123@gmail.com>
2016-12-14 16:15:02 +0100

    SeparatorTestController som funkar

---
+++ Miniproj/Controllers/SeparatorTestController.cs
@@ -0,0 +1,38 @@
+﻿using Miniproj.Models;
+using Miniproj.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Miniproj.Controllers
+{
+    public class SeparatorTestController : ApiController
+    {
+        public SuperRepository repo { get; set; }
+
+        public SeparatorTestController()
+        {
+            repo = new SuperRepository();
+        }
+
+
+        // GET: /api/separatortest/
+        //public SeparatorTestModel Get()
+        [ResponseType(typeof(SeparatorTestModel))]
+        public IHttpActionResult Get()
+        //public Object Get()
+        {
+            SeparatorTestModel text = repo.GetSeparatorTestData();
+
+            //return new { Id = septest.Id, Text = septest.Text };
+            //return new string[] { }
+            //return septest;
+            return Ok(text);
+        }
+
+    }
+}
